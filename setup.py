73df7b448b72ea5a12b8c4c7bc25e78d6c5655ad
Miraclx <omiraculous@gmail.com>
2019-07-05 21:00:07 +0100

    Initialize python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rodio",
+    version="0.1.0",
+    author="Miraculous Owonubi",
+    author_email="omiraculous@gmail.com",
+    description="Crossplatform, non-blocking event loops for async concurrency and I/O",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache-2.0',
+    url="https://github.com/miraclx/node-buffer",
+    packages=['node_events', 'uvloop'],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
