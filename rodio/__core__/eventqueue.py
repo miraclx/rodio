a1ca0dc58460652e7a6e9185b964fa2ac8e84f37
Miraclx <omiraculous@gmail.com>
2019-07-10 01:17:29 +0100

    Hacked together a working eventqueue with pausable iteration

--- rodio/__core__/eventqueue.py
+++ rodio/__core__/eventqueue.py
@@ -6,45 +6,122 @@ Efficient non-blocking event loops for async concurrency and I/O
           Think of this as AsyncIO on steroids
 """
 
+import queue
 import asyncio
+import threading
+from node_events import EventEmitter
 from .internals.debug import debug, debugwrapper
 
 
-class EventQueue(asyncio.Queue):
-    __forceIterStop = __ended = False
+class EventQueue(EventEmitter):
+
     @debugwrapper
     def __init__(self):
         super(EventQueue, self).__init__()
+        self._ended = threading.Event()
+        self._paused = threading.Event()
+        self._running = threading.Event()
+        self._underlayer = queue.Queue()
 
     @debugwrapper
-    def push(self, block):
-        self.put_nowait(block)
+    def push(self, coro, args=()):
+        if self.ended():
+            raise RuntimeError(
+                "Can't schedule executions on a stopped eventqueue")
+        stack = list(coro if isinstance(coro, (tuple, list)) else [coro])
+        notpassed = list(filter(lambda x: not callable(x), stack))
+        if notpassed:
+            raise RuntimeError(
+                f"{notpassed} item{' defined must' if notpassed == 1 else 's defined must all'} either be a coroutine function or a callable object")
+        [stack, typeid] = self.__checkAll(asyncio.iscoroutinefunction, stack, [stack, 1]) or \
+            self.__checkAll(callable, stack, [stack, 0])
+        self._underlayer.put_nowait([stack, args, typeid])
+        if self.paused():
+            self._resume()
 
-    async def __stripCoros(self):
-        debug('async __stripCoros init')
-        while not self.empty() and not self.ended:
-            yield await self.get()
-        self.__ended = True
-        debug('async __stripCoros exit')
+    @debugwrapper
+    def __stripCoros(self):
+        while not self.ended():
+            try:
+                self._running.wait()
+                yield self._underlayer.get_nowait()
+            except queue.Empty:
+                if not self.ended():
+                    self._pause()
 
     async def _startIterator(self):
         debug('async __startIterator init')
-        async for [coro, args] in self.__stripCoros():
-            await coro if asyncio.iscoroutine(coro) else await coro(*args) if asyncio.iscoroutinefunction(coro) else await asyncio.gather(*coro) if isinstance(coro, (list, tuple)) else coro(*args)
-            self.task_done()
+        for [stack, args, typeid] in self.__stripCoros():
+            if typeid == 0:
+                [fn(*args) for fn in stack]
+            elif typeid == 1:
+                await asyncio.gather(*[corofn(*args) for corofn in stack])
+            self._underlayer.task_done()
         debug('async __startIterator exit')
 
+    def _end(self):
+        self.__checkActivityElseRaise()
+        self._running._cond.acquire()
+        self._running._cond.notify()
+        self._running._cond.release()
+        self._ended.set()
+        self._paused.clear()
+        self._running.clear()
+
     @debugwrapper
-    def forceStop(self):
-        if self.__ended:
+    def end(self):
+        self._end()
+        self.emit('end')
+
+    def _pause(self):
+        self.__checkActivityElseRaise()
+        self._paused.set()
+        self._running.clear()
+
+    @debugwrapper
+    def pause(self):
+        self._pause()
+        self.emit('pause')
+
+    def _resume(self):
+        self.__checkActivityElseRaise()
+        self._paused.clear()
+        self._running.set()
+
+    @debugwrapper
+    def resume(self):
+        self._resume()
+        self.emit('resume')
+        return self._startIterator()
+
+    def __checkActivityElseRaise(self):
+        if self.ended():
             raise RuntimeError("Queue iterator already ended")
-        if self.__forceIterStop:
-            raise RuntimeError("Queue iterator already force-stopped")
-        self.__forceIterStop = self.__ended = True
 
-    def closeAll(self):
-        self._queue.clear()
+    def __checkAll(self, fn, iterable, ret=None, msg=None, permissive=True):
+        gen = (fn(blob) for blob in iterable)
+        if any(gen):
+            if not all(gen):
+                raise RuntimeError(
+                    msg or 'A collection of executors must have the same type')
+            else:
+                return ret or True
+        elif not permissive:
+            raise RuntimeError(
+                'Collection of executors must pass the condition')
 
-    @property
+    def paused(self):
+        return self._paused.is_set() and not self._running.is_set()
+
+    isPaused = paused
+    is_paused = paused
+    hasPaused = paused
+    has_paused = paused
+
     def ended(self):
-        return self.__ended or self.__forceIterStop
+        return self._ended.is_set()
+
+    isEnded = ended
+    hasEnded = ended
+    is_ended = ended
+    has_ended = ended
