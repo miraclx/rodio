d5d13a97b084829d500df2973e279a797c20041b
Miraclx <omiraculous@gmail.com>
2019-07-16 02:48:39 +0100

    Made the debugger modular and added script exclusivity for filtering

--- rodio/internals/debug.py
+++ rodio/internals/debug.py
@@ -1,28 +1,40 @@
-def hasArg(arg):
-    from sys import argv
-    clone = [*argv]
-    try:
-        clone.remove(arg)
-        return True
-    except ValueError:
-        return False
+import sys
+from datetime import datetime
 
 
-def debug(fn, *args):
-    from datetime import datetime
-    if (hasArg('--DEBUG')):
-        print(f'[\x1b[33mDEBUG\x1b[0m@\x1b[34m{datetime.now().strftime("%T")}\x1b[0m] [\x1b[32m{fn}\x1b[0m]%s' % (
-            f': {", ".join(map(str, args))}' if len(args) else ''))
+def hasArg(*args):
+    return any(any(v.strip().endswith(x) for x in args) for v in sys.argv)
 
 
-def debugwrapper(start=1, end=None):
-    def wrapper(fn):
-        def underlayer(*args, **kwargs):
-            debug(f'{fn.__qualname__}() init', *args[start:end])
-            fn(*args, **kwargs)
-            debug(f'{fn.__qualname__}() exit')
-        return underlayer
-    if callable(start):
-        [start, fn] = [1, start]
-        return wrapper(fn)
-    return wrapper
+debugLoggers = {}
+
+
+class LogDebugger:
+    def __init__(self, identifier=None):
+        if identifier in debugLoggers:
+            raise ValueError(
+                "The identifier for the debugger already exists within the stack")
+        self.__debug_id__ = identifier
+        debugLoggers[identifier] = self
+
+    def log(self, fn, *args):
+        if hasArg('--DEBUG', *(f'--DEBUG={self.__debug_id__}',) if self.__debug_id__ else ()):
+            print(f'[\x1b[33mDEBUG\x1b[0m@\x1b[34m{datetime.now().strftime("%T")}\x1b[0m] [\x1b[32m{fn}\x1b[0m]%s' % (
+                f': {", ".join(map(str, args))}' if len(args) else ''))
+
+    def debugwrapper(self, start=1, end=None):
+        def wrapper(fn):
+            def underlayer(*args, **kwargs):
+                self.log(f'{fn.__qualname__}() init', *args[start:end])
+                fn(*args, **kwargs)
+                self.log(f'{fn.__qualname__}() exit')
+            return underlayer
+        if callable(start):
+            [start, fn] = [1, start]
+            return wrapper(fn)
+        return wrapper
+
+
+MainLogger = LogDebugger("")
+debug = MainLogger.log
+debugwrapper = MainLogger.debugwrapper
