2ba9b866b2892e9d2e51ebc69fbb985cfb512579
Miraclx <omiraculous@gmail.com>
2019-07-22 07:49:05 +0100

    Added example usage of the `with` statement and the `block` feature flag

---
+++ examples/unblockedwith.py
@@ -0,0 +1,24 @@
+
+import time
+from rodio import EventLoop, get_current_loop, printfromprocess
+
+
+def decoy():
+    loop = get_current_loop()
+    printfromprocess("heyyo from decoy")
+    printfromprocess("sleeping for 2 secs...")
+    time.sleep(2)
+    printfromprocess("quiting after 3 secs...")
+    time.sleep(3)
+    loop.scheduleStop()
+
+
+printfromprocess("main init")
+
+with EventLoop(block=False) as loop:
+    loop.nextTick(lambda: printfromprocess("hello"))
+    loop.nextTick(lambda: printfromprocess("world"))
+    loop.nextTick(decoy)
+
+
+printfromprocess("main exit")
