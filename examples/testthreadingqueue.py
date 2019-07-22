9d9563d9a9417f85b17e8e7e793e3ba27290ab9c
Miraclx <omiraculous@gmail.com>
2019-07-22 07:46:43 +0100

    Test for multithreaded eventloop queueing

---
+++ examples/testthreadingqueue.py
@@ -0,0 +1,26 @@
+import threading
+from rodio import EventLoop, get_current_thread
+
+process = EventLoop()
+
+process.start()
+
+
+def queuedFn(name):
+    print(f"hello from queued function by {name}")
+
+
+def queueDecoy():
+    thread = get_current_thread()
+    print(f"queue from {thread.name}")
+    process.nextTick(queuedFn, thread.name)
+
+
+threads = []
+for i in range(10):
+    thread = threading.Thread(name=f"thread{i}", target=queueDecoy)
+    thread.start()
+    threads.append(thread)
+
+[thread.join() for thread in threads]
+process.scheduleStop()
