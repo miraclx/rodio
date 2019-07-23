caac4a3843f56a333888d49efb96f28d51f2a5ad
Miraclx <omiraculous@gmail.com>
2019-07-23 06:49:10 +0100

    Update example's __main__ script for permanence against format

--- examples/modules/__main__.py
+++ examples/modules/__main__.py
@@ -3,21 +3,24 @@ import posixpath as path
 sys.path.append(path.normpath(path.join(path.dirname(__file__), '../..')))
 
 
-from rodio.internals.parther import getTransformer
-from rodio import EventLoop, printfromprocess
+def init():
+    from rodio.internals.parther import getTransformer
+    from rodio import EventLoop, printfromprocess
+
+    tx = getTransformer(__file__)
+
+    printfromprocess("__main__.py init")
+
+    process1 = EventLoop("process1", self_pause=False)
+    process2 = EventLoop("process2", self_pause=False)
+
+    process1.load_module(tx('./module01.py'))
+    process2.load_module(tx('./module02.py'))
+
+    process1.kill()
+    process2.kill()
+
+    printfromprocess("__main__.py end")
 
 
-tx = getTransformer(__file__)
-
-printfromprocess("__main__.py init")
-
-process1 = EventLoop("process1", self_pause=False)
-process2 = EventLoop("process2", self_pause=False)
-
-process1.load_module(tx('./module01.py'))
-process2.load_module(tx('./module02.py'))
-
-process1.scheduleStop()
-process2.scheduleStop()
-
-printfromprocess("__main__.py end")
+init()
