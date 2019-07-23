fda3f750d9a4b842dc652d55903c7f9f549e1e4a
Miraclx <omiraculous@gmail.com>
2019-07-23 06:46:23 +0100

    Commit update to debugwrapper used by rodio's EventLoop schedule method

--- rodio/internals/debug.py
+++ rodio/internals/debug.py
@@ -26,16 +26,20 @@ class LogDebugger:
             print(f'[\x1b[33mDEBUG\x1b[0m@\x1b[34m{datetime.now().strftime("%T")}\x1b[0m]{idslot}[\x1b[32m{fn}\x1b[0m]%s' % (
                 f': {", ".join(map(str, args))}' if len(args) else ''))
 
-    def debugwrapper(self, start=1, end=None):
+    def debugwrapper(self, start=1, end=None, fn_name=None):
         def wrapper(fn):
+            xfn_name = fn_name or fn.__qualname__
+
             def underlayer(*args, **kwargs):
-                self.log(f'{fn.__qualname__}() init', *args[start:end])
+                self.log(f'{xfn_name}() init', *args[start:end])
                 fn(*args, **kwargs)
-                self.log(f'{fn.__qualname__}() exit')
+                self.log(f'{xfn_name}() exit')
             return underlayer
         if callable(start):
             [start, fn] = [1, start]
             return wrapper(fn)
+        if isinstance(start, str):
+            [fn_name, start, end] = [start, end or 1, fn_name or end]
         return wrapper
 
 
