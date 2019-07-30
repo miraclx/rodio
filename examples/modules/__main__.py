49dd35e1d286d4f7ce60ffc838b2bb783223d810
Miraclx <omiraculous@gmail.com>
2019-07-30 04:07:56 +0100

    Updated modular example to reflect shared_queue feature

--- examples/modules/__main__.py
+++ examples/modules/__main__.py
@@ -6,10 +6,9 @@ sys.path.append(path.normpath(path.join(path.dirname(__file__), '../..')))
 def init():
     from rodio.internals.parther import getTransformer
     from rodio import EventLoop, printfromprocess
-
     tx = getTransformer(__file__)
     printfromprocess("__main__.py init")
-    process = EventLoop("MainEventLoop", self_pause=False)
+    process = EventLoop("MainEventLoop", self_pause=False, shared_queue=False)
     process.load_module(tx('./mainmod.py'), block=True)
     process.terminate()
     printfromprocess("__main__.py end")
--- examples/modules/mainmod.py
+++ examples/modules/mainmod.py
@@ -1,10 +1,11 @@
 from rodio.internals.parther import getTransformer
-from rodio import get_running_loop, EventLoop, is_within_loop
+from rodio import get_running_loop, EventLoop, is_within_loop, printfromprocess
 
 tx = getTransformer(__file__)
 
-process1 = EventLoop(f"process1", self_pause=False)
-process2 = EventLoop(f"process2", self_pause=False)
+printfromprocess("mainmod.py init")
+process1 = EventLoop(f"process1", self_pause=False, shared_queue=False)
+process2 = EventLoop(f"process2", self_pause=False, shared_queue=False)
 
 module1 = process1.load_module(tx('./module01.py'))
 module2 = process2.load_module(tx('./module02.py'))
@@ -17,3 +18,5 @@ module2.on('complete', lambda process: (
 
 process1.join()
 process2.join()
+
+printfromprocess("mainmod.py exit")
